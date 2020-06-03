4468e6c69e7700898b57d225a04f0ed70bb60346
Held Grijo <held.grijo@gmail.com>
2020-06-03 19:46:14 -0400

    Updating routes with params

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,34 @@
 import express, { response } from "express";
 const app = express();
 
+app.use(express.json());
+
+const users = ["Diego", "Cleiton", "Robson", "Daniel"];
+
+// usando query params para filtros/paginação
 app.get("/users", (request, response) => {
-  console.log("listagem de usuários");
-  response.json(["Diego", "Cleiton", "Robson", "Daniel"]);
+  const search = String(request.query.search);
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+  return response.json(filteredUsers);
+});
+
+// usando request params
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const user = users[id];
+  return response.json(user);
+});
+
+// usando request body para atualizar informações
+app.post("/users", (request, response) => {
+  const data = request.body;
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+  return response.json(user);
 });
 
 app.listen(3333);
