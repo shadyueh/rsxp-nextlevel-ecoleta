cfc73ee76cbc1ff2bdbc8a634ffe5df8576cf136
Held Grijo <held.grijo@gmail.com>
2020-06-03 21:56:32 -0400

    Starting App. Configuring database connection.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,34 +1,8 @@
-import express, { response } from "express";
+import express from "express";
+import routes from "./routes";
 const app = express();
 
 app.use(express.json());
-
-const users = ["Diego", "Cleiton", "Robson", "Daniel"];
-
+app.use(routes);
 // usando query params para filtros/paginação
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-  return response.json(filteredUsers);
-});
-
-// usando request params
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const user = users[id];
-  return response.json(user);
-});
-
-// usando request body para atualizar informações
-app.post("/users", (request, response) => {
-  const data = request.body;
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-  return response.json(user);
-});
-
 app.listen(3333);
