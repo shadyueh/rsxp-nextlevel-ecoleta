a7451c65c640a6d0aa38b5a5c467565ed323c6eb
Held Grijo <held.grijo@gmail.com>
2020-06-03 23:59:48 -0400

    Creating migrations using knex

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,15 @@
+import Knex from "knex";
+
+const tableName = "point_items";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable(tableName, (table) => {
+    table.increments("id").primary();
+    table.integer("point_id").notNullable().references("id").inTable("points");
+    table.integer("item_id").notNullable().references("id").inTable("items");
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(tableName);
+}
