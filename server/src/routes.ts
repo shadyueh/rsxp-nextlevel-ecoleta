0fd8eb90b217b8e348cbc2ecfa916c0fcccab229
Held Grijo <held.grijo@gmail.com>
2020-06-05 18:19:15 -0400

    Routes index and show for Points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express, { request } from "express";
+import express from "express";
 
 import ItemsController from "./controllers/ItemsController";
 import PointsController from "./controllers/PointsController";
@@ -8,7 +8,8 @@ const itemsController = new ItemsController();
 const pointsController = new PointsController();
 
 routes.get("/items", itemsController.index);
-
 routes.post("/points", pointsController.create);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
 
 export default routes;
