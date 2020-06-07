365495e1ed8e5f2fd924467261688758f61e2398
Held Grijo <held.grijo@gmail.com>
2020-06-07 13:38:14 -0400

    Setting image for point

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,5 +8,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
-// usando query params para filtros/paginação
 app.listen(3333);
