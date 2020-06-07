a7192aa96253de9f086eb174c2ca0e9fa16e6603
Held Grijo <held.grijo@gmail.com>
2020-06-07 15:42:17 -0400

    Concepts: state, properties

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-
+import Header from "./Header";
 function App() {
-  return <h1>Hello React</h1>;
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <Header title={`Contador: ${counter}`} />
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>
+        Aumentar
+      </button>
+    </div>
+  );
 }
 
 export default App;
