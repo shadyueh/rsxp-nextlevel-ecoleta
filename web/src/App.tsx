f4b055f58930b77430e56b02cdc00a686c7e1d64
Held Grijo <held.grijo@gmail.com>
2020-06-07 22:09:53 -0400

    Creating home and create point pages and routing system

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Header from "./Header";
+import Routes from "./routes";
+
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
-  return (
-    <div>
-      <Header title={`Contador: ${counter}`} />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>
-        Aumentar
-      </button>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
